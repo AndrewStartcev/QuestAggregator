a2933705df756ff122f7a21cfe0290e70c55ceb8
Андрей <40750040+AndrewStartcev@users.noreply.github.com>
2023-01-16 14:22:35 +0800

    add function theme

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -139,11 +139,11 @@ addEventListener("resize", (event) => {
 });
 
 search_btn.addEventListener("click", searchChange);
-document.addEventListener('keydown', function (e) {
-  if (e.which == 27 || window.innerWidth > 1023) {
-    _slideDown(search_form, 100, 'none');
-  }
-});
+// document.addEventListener('keydown', function (e) {
+//   if (e.which == 27 || window.innerWidth > 1023) {
+//     _slideDown(search_form, 100, 'none');
+//   }
+// });
 
 function searchChange() {
   if (window.innerWidth > 1023) {
@@ -919,3 +919,88 @@ if (document.querySelector("#similar_slider")) {
     },
   });
 }
+
+
+// ======================== Смена темы сайnf ================================
+const lightStyles = document.querySelectorAll('link[rel=stylesheet][media*=prefers-color-scheme][media*=light]');
+const darkStyles = document.querySelectorAll('link[rel=stylesheet][media*=prefers-color-scheme][media*=dark]');
+const darkSchemeMedia = matchMedia('(prefers-color-scheme: dark)');
+const switcherRadios = document.querySelectorAll('.switcher__radio');
+
+function setupSwitcher() {
+  const savedScheme = getSavedScheme();
+
+  if (savedScheme !== null) {
+    const currentRadio = document.querySelector(`.switcher__radio[value=${savedScheme}]`);
+    currentRadio.checked = true;
+  }
+
+  [...switcherRadios].forEach((radio) => {
+    radio.addEventListener('change', (event) => {
+      setScheme(event.target.value);
+    });
+  });
+}
+
+function setupScheme() {
+  const savedScheme = getSavedScheme();
+  const systemScheme = getSystemScheme();
+
+  if (savedScheme === null) return;
+
+  if (savedScheme !== systemScheme) {
+    setScheme(savedScheme);
+  }
+}
+
+function setScheme(scheme) {
+  switchMedia(scheme);
+
+  if (scheme === 'auto') {
+    clearScheme();
+  } else {
+    saveScheme(scheme);
+  }
+}
+
+function switchMedia(scheme) {
+  let lightMedia;
+  let darkMedia;
+
+  if (scheme === 'auto') {
+    lightMedia = '(prefers-color-scheme: light)';
+    darkMedia = '(prefers-color-scheme: dark)';
+  } else {
+    lightMedia = (scheme === 'light') ? 'all' : 'not all';
+    darkMedia = (scheme === 'dark') ? 'all' : 'not all';
+  }
+
+  [...lightStyles].forEach((link) => {
+    link.media = lightMedia;
+  });
+
+  [...darkStyles].forEach((link) => {
+    link.media = darkMedia;
+  });
+}
+
+function getSystemScheme() {
+  const darkScheme = darkSchemeMedia.matches;
+
+  return darkScheme ? 'dark' : 'light';
+}
+
+function getSavedScheme() {
+  return localStorage.getItem('color-scheme');
+}
+
+function saveScheme(scheme) {
+  localStorage.setItem('color-scheme', scheme);
+}
+
+function clearScheme() {
+  localStorage.removeItem('color-scheme');
+}
+
+setupSwitcher();
+setupScheme();
